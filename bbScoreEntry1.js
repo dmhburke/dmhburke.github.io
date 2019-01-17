b5598086cff326b7cc45cc443ecb397387a88b8c
dmhburke <44954878+dmhburke@users.noreply.github.com>
2019-01-17 18:56:08 -0500

    Add files via upload

--- bbScoreEntry1.js
+++ bbScoreEntry1.js
@@ -1,1 +1,16 @@
 
+let openScore1 = document.querySelector(".holeEntry");
+let scoreEntryPage1 = document.querySelector(".scoreEntryOne");
+let submitScore1 = document.querySelector(".submitCheckMark");
+
+function scoreEntry1() {
+  scoreEntryPage1.style.visibility = "visible"
+}
+
+openScore1.onclick = scoreEntry1;
+
+function recordScore1() {
+  scoreEntryPage1.style.visibility = "hidden"
+}
+
+submitScore1.onclick = recordScore1;
