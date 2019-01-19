a2cf21b0089013ab2ac57ca382ed28212ce9d3d7
dmhburke <44954878+dmhburke@users.noreply.github.com>
2019-01-19 15:24:31 -0500

    Add files via upload

--- bbScoreEntry1.js
+++ bbScoreEntry1.js
@@ -1,3 +1,17 @@
+//ADD PLAYER NAMES TO SCORECARD//
+let Player1 = 'Daniel Burke';
+
+function populateScorecard() {
+document.getElementById("Player1").innertext = 'Daniel Burke';
+}
+
+
+
+
+
+
+
+
 //SHOW LEADERBOARD//
 
 let scoreboard = document.querySelector(".scoreboardRound1");
@@ -20,14 +34,28 @@ showScoreboard.onclick = scoreboardToggle;
 let openScore1 = document.querySelector(".holeEntry");
 let scoreEntryPage1 = document.querySelector(".scoreEntryOne");
 let submitScore1 = document.getElementById("submitCheckMark");
+let spinnerIcon = document.querySelector(".lds-hourglass");
 
 function scoreEntry1() {
   scoreEntryPage1.style.visibility = "visible"
+  submitScore1.style.visibility = "visible";
+  }
 
-}
 openScore1.onclick = scoreEntry1;
 
-function recordScore1() {
+function scoreEntryClose() {
   scoreEntryPage1.style.visibility = "hidden"
 }
+
+function  spinnerGo() {
+  spinnerIcon.style.visibility = "hidden"
+}
+
+function recordScore1() {
+  submitScore1.style.visibility = "hidden"
+  spinnerIcon.style.visibility = "visible"
+  setTimeout(scoreEntryClose,1500);
+  setTimeout(spinnerGo,1500);
+  }
+
 submitScore1.onclick = recordScore1;
